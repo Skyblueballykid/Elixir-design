2c2acab970934e8e6e9e42749fc47f622d102ec3
Thomas Kalnik <tkalnik@dynamosoftware.com>
2019-05-22 15:38:42 -0500

    Add files via upload

--- github/genserver-test.exs
+++ github/genserver-test.exs
@@ -1,1 +1,22 @@
+defmodule ShoppingList do
+  use GenServer
 
+  # Client API
+
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, [])
+  end
+
+  def read(pid) do
+    GenServer.call(pid, {:read})
+  end
+
+  def add(pid, item) do
+    GenServer.cast(pid, {:add, item})
+  end
+
+
+  # Server Callbacks
+
+
+end
